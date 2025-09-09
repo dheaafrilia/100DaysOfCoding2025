41e6922ce2983ac99f64af3d136eb16ab36d3cc6
dheaafrilia <apriliadea166@gmail.com>
2025-09-09 18:58:56 +0800

    Day2.java

---
+++ Day2.java
@@ -0,0 +1,19 @@
+package day2;
+
+public class Main {
+
+public static void main(String[] args {
+
+  System.out.printl("dhea");
+  System.out.printl("aya");
+
+  System.out.print("informatics");
+  System.out.print("isc");
+
+  int umur = 18;
+  double beraf = 45.1;
+  System.out.printf("umur %d, berat %f" , umur, berat); 
+
+}
+
+}

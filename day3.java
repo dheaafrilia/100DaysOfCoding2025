3eadc178918f198cb7dbb1d5218a385561e86e34
dheaafrilia <apriliadea166@gmail.com>
2025-09-10 20:26:46 +0800

    day3.java

---
+++ day3.java
@@ -0,0 +1,9 @@
+// dhea cantik
+       /* dhea
+       cantik
+       */
+        
+       
+      /* keterangan: (//) hanya komentar dengan satu baris
+                     (/*) komentar yang bisa lebih dari satu baris
+       */

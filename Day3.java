0eda0c9a0df1e9102ba1941b6322bde98080dda2
dheaafrilia <apriliadea166@gmail.com>
2025-09-10 20:48:20 +0800

    Create Day3.java

---
+++ Day3.java
@@ -0,0 +1,18 @@
+package day3;
+
+public class Day3 {
+
+    public static void main(String[] args) {
+        
+       // dhea cantik
+       
+       /* dhea
+       cantik
+       */
+        
+      /* keterangan: (//) hanya komentar dengan satu baris
+                     (/*) komentar yang bisa lebih dari satu baris
+       */
+    }
+    
+}

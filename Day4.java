fbacf6735c12de2f897de92dda81616de6014812
dheaafrilia <apriliadea166@gmail.com>
2025-09-11 22:15:43 +0800

    Day4.java

---
+++ Day4.java
@@ -0,0 +1,24 @@
+package dheaafrilia;
+
+public class Dheaafrilia {
+
+        public static void main(String[] args) {
+            System.out.println("ig\t dheaafrilia__");
+            // (\t) untuk merapikan dokumen sama seperti tab
+            System.out.println("dheaf\b atau");
+            // (\b) untuk menghilangkan 1 kata dibelakang kode 
+            System.out.println("dheaaa \r ayaa");
+            // (\r) untuk menghilangkan semua kata dibelakang kode 
+            System.out.println("dhea\\deya\\aya");
+            // untuk menghilangkan 1 slesh terbalik
+            System.out.println("ig :\'dheaafrilia__\'");
+            // untuk menampilkan tanda kutip 1
+            System.out.println("ig:\"dheaafrilia__\"");
+            // untuk menampilkan tanda kutip 2
+            System.out.println("\u0064\u0068\u0065\u0061");
+            // untuk mengubah kode unicode menjadi teks
+            
+        
+        }
+    
+}

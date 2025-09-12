89831f69042b3a4bbcbd4ced8ee933d5ad364d95
dheaafrilia <apriliadea166@gmail.com>
2025-09-12 21:22:05 +0800

    Day5.java

---
+++ Day5.java
@@ -0,0 +1,26 @@
+package dheaaaaday5;
+
+public class DheaaaaDay5 {
+
+    public static void main(String[] args) {
+    
+    byte angka1 = 1;
+    short angka2 = 2;
+    int angka3 = 3;
+    long angka4 = 4;
+    
+        System.out.println(angka1);
+        //(byte) untuk bilangan bulat yang kecil
+        
+        System.out.println(angka2);
+        //(short) sama saja tapi lebih besar dari byte,
+        
+        System.out.println(angka3);
+        //(int) yang sering digunakan dan lebih besar dari byte dan short,
+        
+        System.out.println(angka4);
+        //(long) paling besar dari tipe data bilangan bulat,
+
+    }
+    
+}

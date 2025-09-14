4743120724ecec886cb9d9497b93140a1ecd492a
dheaafrilia <apriliadea166@gmail.com>
2025-09-14 20:21:35 +0800

    Create Day7.java

---
+++ Day7.java
@@ -0,0 +1,19 @@
+package liaaaaaaaaaa;
+
+public class Liaaaaaaaaaa {
+
+    public static void main(String[] args) {
+        
+     boolean benar = true;
+     boolean salah = false;
+      // boolean untuk menentukan true/false
+      
+     char gender = 'P';
+     // char digunakan untuk penggunaan satu karakter 
+     
+        System.out.println(true);
+        System.out.println(false);
+        System.out.println(gender);
+    }
+    
+}

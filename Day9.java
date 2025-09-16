b11e54769a9ec114b3046de2c2f1923109bd7085
dheaafrilia <apriliadea166@gmail.com>
2025-09-16 18:52:58 +0800

    Day9.java

---
+++ Day9.java
@@ -0,0 +1,16 @@
+package ayaaaw;
+
+public class Ayaaaw {
+
+    public static void main(String[] args) {
+int NilaiAwal,NilaiUpdate,NilaiAkhir;
+NilaiAwal = 70;
+NilaiUpdate = 85;
+NilaiAkhir = NilaiAwal + NilaiUpdate;
+
+        System.out.println("HargaAwal \t= " +NilaiAwal);
+        System.out.println("HargaUpdate \t= " +NilaiUpdate);
+        System.out.println("HargaAkhir \t=" +NilaiAkhir);
+    }
+    
+}

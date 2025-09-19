98addf5973e4c700d29cd23c0f3b910f4bf36a36
dheaafrilia <apriliadea166@gmail.com>
2025-09-19 23:43:21 +0800

    Create Day12.java

---
+++ Day12.java
@@ -0,0 +1,23 @@
+package day12dhea;
+
+public class Day12Dhea {
+
+    public static void main(String[] args) {
+         String nama,nim,alamat,tempat;
+       int tanggal,tahun;
+        
+       nama = "Dhea Afrilia";
+       nim = "D0225340";
+       alamat = "Polewali";
+       tempat = "Polman";
+       tanggal = 15;
+       tahun = 2007;
+       
+        System.out.println("BIODATA DIRI");
+        System.out.println("Nama Lengkap \t: " + nama);
+        System.out.println("NIM \t\t: " + nim);
+        System.out.println("Alamat \t\t: " + alamat);
+        System.out.println("TTL \t\t: "+tempat+"\\"+tanggal+" April "+ tahun); 
+    }
+    
+}

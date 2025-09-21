358c4d18009150d2f8bc2d5fe4613c32cc67b9cf
dheaafrilia <apriliadea166@gmail.com>
2025-09-21 22:49:41 +0800

    Create Day14.java

---
+++ Day14.java
@@ -0,0 +1,19 @@
+package liaaaaaaaaaa;
+
+public class Day14 {
+    public static void main(String[] args) {
+        
+        String Umur = "19";
+        int Umur1 = Integer.parseInt(Umur); 
+        System.out.println(Umur1);
+        
+        String Tinggi = "165.0";
+        double Tinggi1 = Double.parseDouble(Tinggi); 
+        System.out.println(Tinggi1);
+        
+        String benar = "true";
+        boolean benar1 = Boolean.parseBoolean(benar);
+        System.out.println(benar1);
+    }
+  
+}

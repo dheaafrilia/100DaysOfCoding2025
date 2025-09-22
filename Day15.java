3ed45725e6ac864940843fce95dcb8461761fe7a
dheaafrilia <apriliadea166@gmail.com>
2025-09-22 23:21:34 +0800

    Create Day15.java

---
+++ Day15.java
@@ -0,0 +1,16 @@
+package dhea15;
+
+public class Dhea15 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 5;
+
+        int c = a + b;
+        int d = a - b;
+
+        System.out.println("Hasil Penjumlahan: " + c);
+        System.out.println("Hasil Pengurangan: " + d);
+
+    }
+    
+}

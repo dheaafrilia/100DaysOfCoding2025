f63913b09ef7558d79a04a9a4d8b3b170dff301b
dheaafrilia <apriliadea166@gmail.com>
2025-09-23 20:35:04 +0800

    Create Day16.java

---
+++ Day16.java
@@ -0,0 +1,14 @@
+package DDP_2025;
+
+import java.util.Scanner;
+
+public class DheaDay16 {
+    public static void main(String[] args) {
+        Scanner  dhea = new Scanner(System.in);
+        double angka1 = dhea.nextDouble();
+        double angka2 = dhea.nextDouble();
+        System.out.println("perkalian = "+ (angka1*angka2));
+        System.out.println("pembagian = "+ (angka1/angka2));
+        
+    }
+}

07c4a7f32b25c7945231d3baa738318c526399f9
dheaafrilia <apriliadea166@gmail.com>
2025-09-25 23:43:45 +0800

    Create Day18.java

---
+++ Day18.java
@@ -0,0 +1,12 @@
+package day018;
+public class Day018 {
+    public static void main(String[] args) {
+        int angkaint = 25;
+        double angkadouble = angkaint;
+        
+        System.out.println("nilai int = "+ angkaint);
+        System.out.println("nilai double = "+angkadouble);
+
+        
+    }        
+}

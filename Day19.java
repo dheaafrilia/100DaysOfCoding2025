bf77637f1704258644cad171bd46753e1b03de5f
dheaafrilia <apriliadea166@gmail.com>
2025-09-26 22:34:27 +0800

    Create Day19.java

---
+++ Day19.java
@@ -0,0 +1,18 @@
+package konversipaksa;
+
+public class dhea {
+    public static void main(String[] args) {
+        long a = 12345;
+        int b = (int)a;
+        System.out.println(a);
+        
+        short c = 24;
+        byte d = (byte)c;
+        System.out.println(d);
+  
+        double e = 4;
+        float f = (float)e;
+        System.out.println(f);
+
+    }
+}

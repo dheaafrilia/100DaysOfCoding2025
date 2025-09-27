63f6a466565a812a1f58ac5ad7396a4d0578ec4e
dheaafrilia <apriliadea166@gmail.com>
2025-09-27 21:14:41 +0800

    Create Day20.java

---
+++ Day20.java
@@ -0,0 +1,25 @@
+public class DheaJava {
+
+    public static void main(String[] args) {
+                
+        
+        short umur1 = 18;
+        long berat1 = 44;
+        float tinggi1 = 155.1f;        
+        char gender = 'p';
+        
+        String e = umur1 + "";
+        String f = berat1 + "";
+        String g = tinggi1 + "";
+        String h = gender + "";
+        
+        System.out.println("short  ->  String : " + e);
+        System.out.println("long  ->  String : " + f);
+        System.out.println("float  ->  String : " + g);
+        System.out.println("char  ->  String : " + h);
+
+
+
+    }
+
+}

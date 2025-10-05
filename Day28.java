d79b5f64774cf0f0d2146bb39a5dddf1ce6a5b23
dheaafrilia <apriliadea166@gmail.com>
2025-10-05 22:02:47 +0800

    Create Day28.java

---
+++ Day28.java
@@ -0,0 +1,15 @@
+package dhea.java;
+public class DheaJava {
+
+    public static void main(String[] args) {
+       
+        int a = 10;
+        int b = 5;
+        
+        System.out.println(a == b); // false
+        System.out.println(a != b); // true
+        
+
+    }
+
+}

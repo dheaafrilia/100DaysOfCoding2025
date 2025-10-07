3fdd169d0e783b145c42cf19ce937a44162c1607
dheaafrilia <apriliadea166@gmail.com>
2025-10-07 22:01:43 +0800

    Create Day30.java

---
+++ Day30.java
@@ -0,0 +1,15 @@
+package day30;
+
+public class Day30 {
+
+    public static void main(String[] args) {
+
+        int a = 15;
+        int b = 7;
+
+        System.out.println(a >= b);
+        System.out.println(a <= b);
+
+    }
+
+}

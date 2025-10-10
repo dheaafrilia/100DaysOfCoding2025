61f1b60e7c062a50703a8cfc863fd9eea019ba4c
dheaafrilia <apriliadea166@gmail.com>
2025-10-10 22:57:02 +0800

    Create Day33.java

---
+++ Day33.java
@@ -0,0 +1,20 @@
+package Dhea;
+
+import java.util.Scanner;
+
+public class Ayaaaa {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan a : ");
+        int a = in.nextInt();
+
+        boolean b = (a > 0) && (a < 10);
+
+        System.out.println(!b);
+
+    }
+
+}

1e3a84cb21df4806bec3c14f12a076f6b7d54a3b
dheaafrilia <apriliadea166@gmail.com>
2025-10-13 21:38:58 +0800

    Create day36.java

---
+++ day36.java
@@ -0,0 +1,28 @@
+package dhea.java;
+
+import java.util.Scanner;
+
+public class DheaJava {
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Nilai ujian : ");
+        int a = in.nextInt();
+        System.out.print("Kehadiran : ");
+        int b = in.nextInt();
+
+        if (a >= 70) {
+            if (b >= 8) {
+                System.out.println("Selamat Anda lulus");
+            } else {
+                System.out.println("Nilai anda bagus tapi kehadiran anda kurang");
+            }
+        } else {
+            System.out.println("mohon maaf anda tidak lulus");
+        }
+
+
+    }
+
+}

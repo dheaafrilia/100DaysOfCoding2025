0f93123c5951bfcc76295317df9d15e884692589
dheaafrilia <apriliadea166@gmail.com>
2025-10-14 21:51:32 +0800

    Create Day37.java

---
+++ Day37.java
@@ -0,0 +1,24 @@
+package Dheaa;
+
+import java.util.Scanner;
+
+public class Ayaaa {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan Angka : ");
+        int angka = in.nextInt();
+
+        if ( angka % 2 ==0 ) {
+           System.out.println(angka + " Genap")
+        }else {
+           System.out.println(angka + " Ganjil")
+        }
+
+    
+
+    }
+
+}

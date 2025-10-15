d246e1a6ab6032da557e1c85a166a00b7eaa865e
dheaafrilia <apriliadea166@gmail.com>
2025-10-15 22:16:12 +0800

    Create Day38.java

---
+++ Day38.java
@@ -0,0 +1,23 @@
+package dhea;
+
+import java.util.Scanner;
+
+public class Ayaa{
+public static void main(String[]args){
+  Scanner in = new Scanner (System.in);
+System.out.print("Masukkan angka");
+  int a =in.nextInt();
+
+  if (a > 0 ){
+    System.out.println(a +(" Positif");
+  }else if (a < 0 ){
+    System.out.println(a +(" Negatif");
+  }else {
+    System.out.println(a +(" Nol");
+  }
+
+  
+ }
+  
+}
+                       

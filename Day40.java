5773f60628d9e3554a6bc6d29996e49c79da3e8b
dheaafrilia <apriliadea166@gmail.com>
2025-10-17 21:58:36 +0800

    Create Day40.java

---
+++ Day40.java
@@ -0,0 +1,41 @@
+package day40;
+import java.util.Scanner;
+public class java {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        double angka1, angka2, hasil;
+        char operator;
+
+        System.out.println("=== Kalkulator Sederhana ===");
+        System.out.print("Masukkan angka pertama: ");
+        angka1 = in.nextDouble();
+
+        System.out.print("Masukkan operator (+, -, *, /): ");
+        operator = in.next().charAt(0);
+
+        System.out.print("Masukkan angka kedua: ");
+        angka2 = in.nextDouble();
+
+        // Logika menggunakan if
+        if (operator == '+') {
+            hasil = angka1 + angka2;
+            System.out.println("Hasil: " + (int) hasil);
+        } else if (operator == '-') {
+            hasil = angka1 - angka2;
+            System.out.println("Hasil: " + (int) hasil);
+        } else if (operator == '*') {
+            hasil = angka1 * angka2;
+            System.out.println("Hasil: " + (int) hasil);
+        } else if (operator == '/') {
+            if (angka2 != 0) {
+                hasil = angka1 / angka2;
+                System.out.printf("Hasil: %.2f",hasil);
+            } else {
+                System.out.println("Error: Tidak bisa dibagi dengan nol!");
+            }
+        } else {
+            System.out.println("Operator tidak dikenal!");
+        }
+    }
+ 
+}

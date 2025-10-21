b96cac7e562cb70cec4f58c4baf5aa9f1aa8a75c
dheaafrilia <apriliadea166@gmail.com>
2025-10-21 20:41:52 +0800

    Create Day44.java

---
+++ Day44.java
@@ -0,0 +1,27 @@
+package day44.java;
+
+import java.util.Scanner;
+
+public class Day44Java {
+   
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        System.out.print("Masukkan nilai (0 - 100 ) = ");
+        int a = in.nextInt();
+        
+        if (a >= 85 && a <= 100){
+        System.out.println("Grade A");
+    }else if (a >= 75 && a <= 85){
+            System.out.println("Grade B");
+    }else if (a >= 60 && a <= 75){
+            System.out.println("Grade C");
+    }else if (a >= 40 && a <= 60){
+            System.out.println("Grade D");
+    }else if (a >= 0 && a <= 40){
+            System.out.println("Grade E");
+    }else {
+            System.out.println("Nilai Tidak Valid!!");
+    }
+    }
+    
+}

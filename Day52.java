96dfe705bae8ee821eeaff0cd19a5de9d6850095
dheaafrilia <apriliadea166@gmail.com>
2025-10-29 22:43:27 +0800

    Day52.java

---
+++ Day52.java
@@ -0,0 +1,11 @@
+public class day52 {
+    public static void main(String[] args) {
+        //PERULANGAN WHILE
+        int a = 1;
+        while (a <= 10){
+            System.out.println("angka :  " + a);
+            a++;
+        }
+    }
+    
+}

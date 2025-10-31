69da88ca4062ba4209179a42d2bb46a742cc64f1
dheaafrilia <apriliadea166@gmail.com>
2025-10-31 23:13:21 +0800

    Day54.java

---
+++ Day54.java
@@ -0,0 +1,22 @@
+package day54;
+
+public class day54 {
+    
+    public static void main(String[] args) {
+
+        for (int i = 1; i < 10; i++) {
+            if (i == 5) continue ;
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+        int a = 10;
+        while (a > 0) {
+            a--;
+            if(a == 5)continue;
+            System.out.println(a);
+        }
+        
+    }
+
+} 
+    

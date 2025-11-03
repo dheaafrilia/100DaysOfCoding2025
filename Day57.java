6422c0afce4111f6d24d08c9fe98096ec2e82fc8
dheaafrilia <apriliadea166@gmail.com>
2025-11-03 22:02:54 +0800

    Day57.java

---
+++ Day57.java
@@ -0,0 +1,12 @@
+public class day57 {
+    
+    public static void main(String[] args) {
+     
+        for (int i = 1; i <= 3; i++) {
+            System.out.println("Loop i : "+i);
+            for (int j = 1; j <= 2; j++) {
+                System.out.println("  Loop j : "+j);
+            }
+        }
+        }
+    }

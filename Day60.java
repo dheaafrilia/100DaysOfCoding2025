ca063c65897fb455999915cbc83b409fb1d92a03
dheaafrilia <apriliadea166@gmail.com>
2025-11-06 20:47:20 +0800

    Day60.java

---
+++ Day60.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class day60 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+       
+
+        System.out.print("Masukkan N : ");
+        int n = in.nextInt();
+
+        System.out.println("Bilangan Ganjil");
+        for (int i = n; i > 0; i--) {
+            if (i % 2 == 1) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(" ");
+        System.out.println("Bilangan Genap");
+        for (int i = n; i > 0; i--) {
+            if (i % 2 == 0) {
+                System.out.print(i + " ");
+            }
+        }
+        System.out.println(" ");
+    }
+
+}
+    
+

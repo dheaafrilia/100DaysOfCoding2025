75ae43b446d787e56357ef0c941913523876e8e0
dheaafrilia <apriliadea166@gmail.com>
2025-11-07 23:00:59 +0800

    Day61.java

---
+++ Day61.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class day61 {
+    
+    public static void main(String[] args) {
+      
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Masukkan N : ");
+        int n = in.nextInt();
+        System.out.print("Masukkan M : ");
+        int m = in.nextInt();
+
+        System.out.println("Kelipatan "+m+" dari "+n+" adalah : ");
+        for (int i = 1; i <= n; i++) {
+            if (i % m == 0) {
+                System.out.print(i+" ");
+            }
+        }
+        System.out.println(" ");
+    }
+
+}
+    
+ 
+

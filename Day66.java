5f1f5f7c928b2d5bf2731688ae7a131361c02747
dheaafrilia <apriliadea166@gmail.com>
2025-11-12 22:13:51 +0800

    Day66.java

---
+++ Day66.java
@@ -0,0 +1,16 @@
+public class day66 {
+
+    public static void main(String[] args) {
+
+        //LATIHAN : MEMBUAT POLA VERTIKAL
+        
+        System.out.println("===POLA VERTIKAL===");
+
+        int n = 5;
+
+        for (int i = 1; i <= n; i++) {
+            System.out.println("*");
+        }
+    }
+
+}
